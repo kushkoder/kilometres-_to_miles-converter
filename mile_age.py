5baab579bdb8d498116715cb05ed368b85f4d850
kushkoder <31478363+kushkoder@users.noreply.github.com>
2018-08-02 13:16:19 +0530

    Update mile_age.py

--- mile_age.py
+++ mile_age.py
@@ -2,4 +2,4 @@ print("How many kilometres did you cycle today?")
 kms = input()
 miles = float(kms)/1.60934
 miles = round(miles,2) 
-print(f"That is equal to {miles} miles ")
+print(f"Your {kms}km ride was {miles} miles ")
